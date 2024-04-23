6301347dd9f93885d90807d90a3bb53f7d44ec8a
Eduardo <eduardojor77@gmail.com>
2024-04-23 16:40:22 -0300

    adding networks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,32 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    local: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk"
+      }
+    },
+    sepolia: {
+      url: process.env.INFURA_URL,
+      chainId: 11155111,
+      accounts: {
+        mnemonic: process.env.SECRET
+      }
+    },
+    fantom: {
+      url: "https://rpc.ankr.com/fantom_testnet",
+      chainId: 4002,
+      accounts: {
+        mnemonic: process.env.SECRET
+      }
+    }
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY
+  }
 };
 
 export default config;
