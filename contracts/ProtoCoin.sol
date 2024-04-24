880e0ff0361f1ea0bf021d91a61631d9e5721178
Eduardo <eduardojor77@gmail.com>
2024-04-24 14:02:31 -0300

    implemented constructor

--- contracts/ProtoCoin.sol
+++ contracts/ProtoCoin.sol
@@ -1,8 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "openzeppelin/contracts/token/ERC20/ERC20.sol"
+import "openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract ProtoCoin {
+contract ProtoCoin is ERC20{
 
+    constructor() ERC20("ProtoCoin", "PRC") {
+        
+    }
 }
