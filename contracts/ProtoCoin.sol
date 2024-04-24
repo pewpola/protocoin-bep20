f6c650117a594c90918ec6303a133d2f6f66f678
Eduardo <eduardojor77@gmail.com>
2024-04-24 14:08:10 -0300

    mint function

--- contracts/ProtoCoin.sol
+++ contracts/ProtoCoin.sol
@@ -6,6 +6,6 @@ import "openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract ProtoCoin is ERC20{
 
     constructor() ERC20("ProtoCoin", "PRC") {
-        
+        _mint(msg.sender, 1000 * 10 ** 18);
     }
 }
